27b6fe650546c8641b32e54eb6217f159f28c2c5
chibuezedev <paulpadro03@gmail.com>
2026-03-02 12:54:51 +0100

    update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ connectDB();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: "https://mio-vision.vercel.app",
     credentials: true,
   })
 );
